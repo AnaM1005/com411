87ccd5fe4019c54b8a6b749cc40b1c6ee71c0488
repl.it user <replituser@example.com>
2020-10-06 09:50:11 +0000

    Added code to demonstrate performance of an if-else statement

--- basics/decisions/if.py
+++ basics/decisions/if.py
@@ -1,5 +1,10 @@
+#Ask user about the type of book 
 print("What type of books is this?")
 book_type = input()
+
+#Determine if the book is an adventure book
 if (book_type=="adventure"):
  print("I like adventure books!")
+
+#Display message
 print("Finished reading books.")
