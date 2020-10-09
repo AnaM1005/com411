72b42ffc768a622bf6e88a6a431783be9f4bfcd0
repl.it user <replituser@example.com>
2020-10-09 21:16:40 +0000

    Added a code to demonstrate the use of of a logical end operator

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,11 @@
+#Ask the user what did they see and hear
+print("What did I hear?")
+heard=input()
+
+print("What did I see?")
+saw=input()
+
+if ((heard == "grrr") and (saw == "two red eyes")):
+  print("There is a scary creature. I should get our of here!")
+else:
+  print("I am a little scared, but I will continue.")
