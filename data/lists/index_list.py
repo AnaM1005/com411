b2f662ee8c058fdd357147e5016deb5650e8661c
repl.it user <replituser@example.com>
2020-10-22 20:48:00 +0000

    Added code to demonstrate how to index a list.

---
+++ data/lists/index_list.py
@@ -0,0 +1,20 @@
+#Defining the functions movements
+def movements():
+  path = ["Move Forward", 
+          10, 
+          "Move Backward", 
+          5, 
+          "Move Left", 
+          3, 
+          "Move Right", 
+          1]
+  return path
+
+def run():
+  print("Moving...")
+  path = movements()
+  for index in range(0, len(path), 2):
+     print("{} for {} steps.". format (path [index], path[index + 1]))
+  
+#Call to the function
+run()
